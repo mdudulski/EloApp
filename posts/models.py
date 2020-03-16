e7799960a811550b591a1b72c11392fc860b3ffe
Mateusz Dudulsk <mateusz.dudulski@altalog.pl>
2020-03-16 09:51:38 +0100

    initial commit

---
+++ posts/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+
+# Create your models here.
+
+# stworzenie wpisu w bazie danych
+# https://www.youtube.com/watch?v=ExTaxRmDnP8
+# pobieranie elemenetów z Django Admina
+# https://www.youtube.com/watch?v=5jWJBpS0tkg
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def _str_(self):
+        return self.title
+
+
+
+#class Player(models.Model):
+#    PLAYER_CHOICES = (
+#        ('Adam'),('Adam'),
+#        ('Bartek'), ('Bartek'),
+#        ('Paweł'), ('Paweł'),
+#        ('Mateusz'), ('Mateusz'),
+#    )
+#    name = models.CharField(max_length = 100, choices = PLAYER_CHOICES)
